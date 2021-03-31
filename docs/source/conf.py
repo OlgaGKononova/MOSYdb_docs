81e89d52a94aebc8c90a2cac47085b8c27f6cd81
olga <OlgaGKononova@yandex.ru>
2021-03-30 21:56:42 -0700

    - Prettified icons and logo  - Added CSS

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -48,19 +48,24 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
+#html_css_files = [
+#    'css/insegel_upd.css',
+#]
 html_theme = 'insegel'
 html_logo = "MOSYdb.png"
+html_favicon = "MOSYdb-32.png"
 html_theme_options = {
     'display_version': False,
     'wending_last_updated' : False
 }
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_show_sphinx = False
